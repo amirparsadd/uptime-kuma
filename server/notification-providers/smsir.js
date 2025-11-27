e79f6f32ea14cd843df462cea9acfb66567e298b
Amirparsa Baghdadi <amirparsab9@gmail.com>
2025-11-27 13:12:00 +0330

    implemented backend for feature

--- server/notification-providers/smsir.js
+++ server/notification-providers/smsir.js
@@ -32,13 +32,14 @@ class SMSIR extends NotificationProvider {
                     return mobile;
                 });
 
-            const MAX_MESSAGE_LENGTH = 25; // This is a limitation placed by SMSIR
+            const DEFAULT_MAX_MESSAGE_LENGTH = 25; // This is a limitation placed by SMSIR
+            const maxMessageLength = parseInt(notification.smsirMaxLength) ? parseInt(notification.smsirMaxLength) : DEFAULT_MAX_MESSAGE_LENGTH;
             // Shorten By removing spaces, keeping context is better than cutting off the text
             // If that does not work, truncate. Still better than not receiving an SMS
-            if (msg.length > MAX_MESSAGE_LENGTH && msg.replace(/\s/g, ""). length <= MAX_MESSAGE_LENGTH) {
+            if (msg.length > maxMessageLength && msg.replace(/\s/g, ""). length <= maxMessageLength) {
                 msg = msg.replace(/\s/g, "");
-            } else if (msg.length > MAX_MESSAGE_LENGTH) {
-                msg = msg.substring(0, MAX_MESSAGE_LENGTH - 1 - "...".length) + "...";
+            } else if (msg.length > maxMessageLength) {
+                msg = msg.substring(0, maxMessageLength - 1 - "...".length) + "...";
             }
 
             // Run multiple network requests at once
